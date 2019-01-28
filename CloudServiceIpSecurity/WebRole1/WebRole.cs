c7f63da21a10f36cccaf4c699800a4216ea09fd4
Denis Ouspenski <DenisO@phenomenex.com>
2019-01-29 05:17:05 +0700

    .

--- CloudServiceIpSecurity/WebRole1/WebRole.cs
+++ CloudServiceIpSecurity/WebRole1/WebRole.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Hosting;
+using Microsoft.Web.Administration;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
@@ -16,5 +18,20 @@ namespace WebRole1
 
             return base.OnStart();
         }
+
+        void RoleEnvironment_Changing(object sender, RoleEnvironmentChangingEventArgs e)
+        {
+            var configurationChanges = e.Changes
+                .OfType<RoleEnvironmentConfigurationSettingChange>()
+                .ToList();
+
+
+            if (!configurationChanges.Any()) return;
+
+            if (configurationChanges.Any(c => c.ConfigurationSettingName == "StorageAccount"))
+                e.Cancel = true;
+        }
+
+
     }
 }
